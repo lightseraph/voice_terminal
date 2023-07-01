6d71151a29e92d5ce4180ed264c5c3e58f1dfb96
LightBook <lightseraph@163.com>
2023-07-01 20:12:01 +0800

    7.1.1

--- lib/IR/irda.c
+++ lib/IR/irda.c
@@ -3,10 +3,12 @@
 uint8_t g_remote_sta = 0;
 uint32_t g_remote_data = 0; /* 红外接收到的数据 */
 uint8_t g_remote_cnt = 0;   /* 按键按下的次数 */
+uint8_t g_valid_channel = 0;
+const uint8_t REMOTE_ID = 0xBF;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-    if (htim == &htim2)
+    if (htim == &htim3)
     {
         if (g_remote_sta & 0x80) /* 上次有数据被接收到了 */
         {
@@ -32,48 +34,35 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
-    if (htim == &htim2)
+    if (htim == &htim3)
     {
-        uint16_t dval;
-
-        if (IR_A_DATA)
+        if (g_valid_channel != 2)
         {
-            __HAL_TIM_SET_CAPTUREPOLARITY(&htim2, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
-            __HAL_TIM_SET_COUNTER(&htim2, 0);
-        }
-        else
-        {
-            dval = HAL_TIM_ReadCapturedValue(&htim2, TIM_CHANNEL_1);                               /* 读取CCR1也可以清CC1IF标志位 */
-            __HAL_TIM_SET_CAPTUREPOLARITY(&htim2, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING); /* 设置为上升沿捕获 */
-            if (g_remote_sta & 0X10)                                                               /* 完成一次高电平捕获 */
+            if (IR_A_DATA)
             {
-                if (g_remote_sta & 0X80) /* 接收到了引导码 */
-                {
-
-                    if (dval > 300 && dval < 800) /* 560为标准值,560us */
-                    {
-                        g_remote_data >>= 1;            /* 右移一位 */
-                        g_remote_data &= ~(0x80000000); /* 接收到0 */
-                    }
-                    else if (dval > 1400 && dval < 1800) /* 1680为标准值,1680us */
-                    {
-                        g_remote_data >>= 1;         /* 右移一位 */
-                        g_remote_data |= 0x80000000; /* 接收到1 */
-                    }
-                    else if (dval > 2000 && dval < 3000) /* 得到按键键值增加的信息 2500为标准值2.5ms */
-                    {
-                        g_remote_cnt++;       /* 按键次数增加1次 */
-                        g_remote_sta &= 0XF0; /* 清空计时器 */
-                    }
-                }
-                else if (dval > 4200 && dval < 4700) /* 4500为标准值4.5ms */
-                {
-                    g_remote_sta |= 1 << 7; /* 标记成功接收到了引导码 */
-                    g_remote_cnt = 0;       /* 清除按键次数计数器 */
-                }
+                __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
+                __HAL_TIM_SET_COUNTER(htim, 0);
+                g_remote_sta |= 0x10;
+                g_valid_channel = 1;
             }
-
-            g_remote_sta &= ~(1 << 4);
+            else
+            {
+                DataCollect(htim);
+            }
+        }
+        else if (g_valid_channel != 1)
+        {
+            if (IR_B_DATA)
+            {
+                __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_2, TIM_INPUTCHANNELPOLARITY_FALLING);
+                __HAL_TIM_SET_COUNTER(htim, 0);
+                g_remote_sta |= 0x10;
+                g_valid_channel = 2;
+            }
+            else
+            {
+                DataCollect(htim);
+            }
         }
     }
 }
@@ -88,7 +77,7 @@ uint8_t remote_scan(void)
         t1 = g_remote_data;               /* 得到地址码 */
         t2 = (g_remote_data >> 8) & 0xff; /* 得到地址反码 */
 
-        if ((t1 == (uint8_t)~t2) && t1 == REMOTE_ID) /* 检验遥控识别码(ID)及地址 */
+        if ((t1 == (uint8_t)~t2) && t1 == LOCAL_ID[local_id]) /* 检验遥控识别码(ID)及地址 */
         {
             t1 = (g_remote_data >> 16) & 0xff;
             t2 = (g_remote_data >> 24) & 0xff;
@@ -96,6 +85,9 @@ uint8_t remote_scan(void)
             if (t1 == (uint8_t)~t2)
             {
                 sta = t1; /* 键值正确 */
+                g_remote_data = 0;
+                g_remote_sta = 0;
+                g_valid_channel = 0;
             }
         }
 
@@ -107,4 +99,48 @@ uint8_t remote_scan(void)
     }
 
     return sta;
+}
+
+void DataCollect(TIM_HandleTypeDef *htim)
+{
+    uint16_t dval;
+    if (g_valid_channel != 2)
+    {
+        dval = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);                               /* 读取CCR1也可以清CC1IF标志位 */
+        __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING); /* 设置为上升沿捕获 */
+    }
+    else if (g_valid_channel != 1)
+    {
+        dval = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_2);                               /* 读取CCR1也可以清CC1IF标志位 */
+        __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_2, TIM_INPUTCHANNELPOLARITY_RISING); /* 设置为上升沿捕获 */
+    }
+    if (g_remote_sta & 0X10) /* 完成一次高电平捕获 */
+    {
+        if (g_remote_sta & 0X80) /* 接收到了引导码 */
+        {
+
+            if (dval > 400 && dval < 700) /* 560为标准值,560us */
+            {
+                g_remote_data >>= 1;            /* 右移一位 */
+                g_remote_data &= ~(0x80000000); /* 接收到0 */
+            }
+            else if (dval > 1400 && dval < 1800) /* 1680为标准值,1680us */
+            {
+                g_remote_data >>= 1;         /* 右移一位 */
+                g_remote_data |= 0x80000000; /* 接收到1 */
+            }
+            else if (dval > 2200 && dval < 2800) /* 得到按键键值增加的信息 2500为标准值2.5ms */
+            {
+                g_remote_cnt++;       /* 按键次数增加1次 */
+                g_remote_sta &= 0XF0; /* 清空计时器 */
+            }
+        }
+        else if (dval > 4200 && dval < 4700) /* 4500为标准值4.5ms */
+        {
+            g_remote_sta |= 1 << 7; /* 标记成功接收到了引导码 */
+            g_remote_cnt = 0;       /* 清除按键次数计数器 */
+        }
+    }
+
+    g_remote_sta &= ~(1 << 4);
 }
